224892436d91f6d8de52a1cce26ad50ab6e51937
Gurpreet <gurpreetsodhi@aol.com>
2021-05-15 20:56:37 -0400

    Added ContentDetailView Added Constants and url

---
+++ LearningApp/Data/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Sodhis on 5/15/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostURL = "https://codewithchris.github.io/learningJSON/"
+
+}
